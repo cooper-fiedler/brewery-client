e68d7bcdc790fa8f48d23e65dde1b959d5a496f3
cooper-fiedler <103666397+cooper-fiedler@users.noreply.github.com>
2022-10-10 10:53:51 -0700

    Added integration testing to RestTemplate client calls

---
+++ src/main/java/com/cooperfiedler/breweryclient/web/client/package-info.java
@@ -0,0 +1,1 @@
+package com.cooperfiedler.breweryclient.web.client;
--- src/test/java/com/cooperfiedler/breweryclient/web/client/BreweryClientTest.java
+++ src/test/java/com/cooperfiedler/breweryclient/web/client/BreweryClientTest.java
@@ -3,11 +3,14 @@ package com.cooperfiedler.breweryclient.web.client;
 import com.cooperfiedler.breweryclient.web.model.BeerDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import java.net.URI;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@SpringBootTest
 class BreweryClientTest {
 
     @Autowired
@@ -15,9 +18,31 @@ class BreweryClientTest {
 
     @Test
     void getBeerById() {
-
         BeerDto dto = client.getBeerById(UUID.randomUUID());
         assertNotNull(dto);
 
+    }
+
+    @Test
+    void saveNewBeer() {
+        BeerDto beerDto = BeerDto.builder().beerName("New Beer").build();
+
+        URI uri = client.saveNewBeer(beerDto);
+
+        assertNotNull(uri);
+
+        System.out.println(uri.toString());
+    }
+
+    @Test
+    void updatBeer() {
+        BeerDto beerDto = BeerDto.builder().beerName("New Beer").build();
+
+        client.updatBeer(UUID.randomUUID(), beerDto);
+    }
+
+    @Test
+    void deleteBeer() {
+        client.deleteBeer(UUID.randomUUID());
     }
 }
